9909ee5e4aec555604d591281f0cbca8ffd8fab3
Bstuparek <bjoern.stuparek@stud.hcw.ac.at>
2026-01-07 16:51:18 +0100

    Added Sent, Received and Read States to ChatMessage Class

--- src/main/java/at/ac/hcw/simplechattool/ChatMessage.java
+++ src/main/java/at/ac/hcw/simplechattool/ChatMessage.java
@@ -7,11 +7,13 @@ public class ChatMessage {
     private final String content;
     private final int messageID;
     private static int nextMessageID = 1;
+    private int currentState = 0;       //States can be: 0 = not sent, 1 Sent, 2 Received, 3 Read,
 
-    public ChatMessage(String sender, String content){
+    public ChatMessage(String sender, String content, int currentState){
         this.sender = sender;
         this.content = content;
         this.messageID = nextMessageID++;
+        this.currentState = currentState;
 
     }
 
@@ -19,6 +21,20 @@ public class ChatMessage {
     public String getMessage(String sender, String content){
         DateTimeFormatter time = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return String.format("[%s] %s: \n%s", time, sender, content);
+    }
+
+    public void markAsSent(){
+        if(getCurrentState() == 0) setCurrentState(1);
+    }
+
+    public void markAsReceived(){
+        if(getCurrentState() == 1) setCurrentState(2);
+    }
+    public void markAsRead(){
+        if(getCurrentState() == 2) setCurrentState(3);
+    }
+    public void markAsUnread(){
+       if(getCurrentState() > 1) setCurrentState(2);
     }
 
     public String getSender(){
@@ -33,4 +49,11 @@ public class ChatMessage {
         return messageID;
     }
 
+    public int getCurrentState(){
+        return currentState;
+    }
+
+    public void setCurrentState(int state){
+
+    }
 }
